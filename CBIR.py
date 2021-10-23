e665950e9a312f1ac0d1be3685672b6a9f45cb5a
Emily <mrremi007@myuct.ac.za>
2021-10-23 16:26:46 +0200

    Created files for basic functionality

---
+++ CBIR.py
@@ -0,0 +1,71 @@
+import os
+import sys
+
+from ProcessImage import Image
+
+
+# Returns the k best images that match the query image
+def find_images(image_base, query_image_name, num_matches):
+    query_image = Image(query_image_name)
+    # weights for distance calculation
+    # TODO: perhaps change weights
+    weights = [1, 1, 1, 1]
+    print("Calculating distances")
+    for image in image_base:
+        # other weights
+        # TODO: change weights
+        image.image_distance(query_image, weights, 1, 1, 1)
+    image_base.sort(key=lambda img: img.distance)
+    first_processed = 0
+    # find first image that was processed (whose distance was computed) in sorted list
+    print("Finding the ", num_matches, " best image matches")
+    for img in image_base:
+        if img.distance >= 0:
+            break
+        first_processed += first_processed
+    # if not enough images were processed, output random (or maybe throw exception in future)
+    if num_matches + first_processed - 1 >= len(image_base):
+        extra = num_matches + first_processed - len(image_base)
+        print("Not enough processed images, showing", extra, " unprocessed images.")
+        return image_base[first_processed::] + image_base[0:extra:]
+    # otherwise extract the best matches
+    else:
+        return image_base[first_processed:first_processed + num_matches:]
+
+
+def main():
+    if len(sys.argv) <= 1:
+        print("Error: Must enter directory containing images to search")
+    else:
+        image_base = []
+        folder = sys.argv[1]
+        # TODO: error if not image
+        print("Loading image base...")
+        #count = 0
+        for filename in os.listdir(folder):
+            image_base.append(Image(filename))
+            #count += 1
+            #print(count)
+        print("Finished loading images")
+        print(len(image_base))
+
+        while True:
+            option = input("Enter a query image (or 'quit' to exit): ")
+            if option == "quit":
+                break
+            else:
+                # TODO: change beta threshold based on number of matches
+                matches = eval(input("Enter number of matches to search for: "))
+                if matches > len(image_base):
+                    print("Not enough images. Number of requested matches:", matches, "Number of images to search: ",
+                          len(image_base))
+                else:
+                    print("Beginning image retrieval")
+                    matching_images = find_images(image_base, option, matches)
+                    print("Best matching images")
+                    for img in matching_images:
+                        print(img.image_name)
+
+
+if __name__ == '__main__':
+    main()
---
+++ ProcessImage.py
@@ -0,0 +1,119 @@
+import cv2
+import numpy as np
+import pywt
+
+
+# TODO: saving features to JSON
+
+class Image:
+
+    def __init__(self, image_name):
+        self.image_name = image_name
+        print(image_name)
+        # TODO: CHANGE
+        c1, c2, c3 = self.create_axes("/home/emily/Documents/2021/CSC5029Z/MiniProject/JPEGImages/" + image_name)
+        # Feature is array [standard_deviation, dA5, dH5, dV5, dD5, dA4, dH4, dV4, dD4]
+        self.c1_feature = self.create_features(c1)
+        self.c2_feature = self.create_features(c2)
+        self.c3_feature = self.create_features(c3)
+        self.distance = 0
+
+    # Takes in an image name, resizes image and extracts 3 color axes using the formulas:
+    # C1 = (R+G+B)/3
+    # C2 = (R+(max-B))/2
+    # C3 = (R +2*(max-G)+b)/4
+    # Returns 3 matrices, describing each of the color axes
+    # TODO: potentially change to some other axis
+    def create_axes(self, image_name):
+        original = cv2.imread(image_name, cv2.IMREAD_UNCHANGED)
+        # resize image, using interlinear interpolation
+        # TODO: potentially change size and interpolation
+        image = cv2.resize(original, (128, 128), interpolation=cv2.INTER_LINEAR)
+        width, height, components = image.shape
+        c1 = np.zeros((width, height))
+        c2 = np.zeros((width, height))
+        c3 = np.zeros((width, height))
+        red = image[:, :, 2]
+        green = image[:, :, 1]
+        blue = image[:, :, 0]
+        # calculate the axes, values will be real numbers (not necessarily whole numbers)
+        for i in range(width):
+            for j in range(height):
+                c1[i][j] = (red[i][j] + green[i][j] + blue[i][j]) / 3
+                c2[i][j] = (red[i][j] + (255 - blue[i][j])) / 2
+                c3[i][j] = (red[i][j] + 2 * (255 - green[i][j]) + blue[i][j]) / 4
+        return c1, c2, c3
+
+    # Extract features from the color axis of an image as defined in:
+    # "Content-based image indexing and searching using Daubechies' wavelets"
+    # Features:
+    # 1. 16x16 sub-matrix of 4-level DWT: M4
+    # 2. Standard deviation of 8x8 sub-matrix of M4
+    # 3. 8x8 sub-matrix of 5-level DWT: M5
+    def create_features_long(self, color_axis):
+        # leave mode as symmetric, use debauchie 2 wavelets
+        # TODO: Potentially change wavelet type and signal extension method
+        dwt4 = pywt.wavedec2(color_axis, 'db2', level=4)
+        # approximation and detail matrices for level 4
+        dA4 = dwt4[0]
+        (dH4, dV4, dD4) = dwt4[1]
+        standard_deviation = dA4.std()
+        dwt5 = pywt.wavedec2(color_axis, 'db2', level=5)
+        # approximation and detail for level 5
+        dA5 = dwt5[0]
+        (dH5, dV5, dD5) = dwt5[1]
+        return standard_deviation, dA5, dH5, dV5, dD5, dA4, dH4, dV4, dD4
+
+    def create_features(self, color_axis):
+        # leave mode as symmetric, use debauchie 2 wavelets
+        # TODO: Potentially change wavelet type and signal extension method
+        dwt4 = pywt.wavedec2(color_axis, 'db2', level=4)
+        # approximation and detail matrices for level 4
+        dA4 = dwt4[0]
+        (dH4, dV4, dD4) = dwt4[1]
+        standard_deviation = dA4.std()
+        return standard_deviation, dA4, dH4, dV4, dD4
+
+    # Filter for standard deviation before
+    # TODO: Filter based on level-5
+    # Perhaps generalize for level-5 decomposition
+    # weights = [w11, w12, w21, w22]
+    def image_distance_long(self, query_image, weights, wc1, wc2, wc3, percent=50):
+        beta = 1 - percent / 100
+        # filter based on standard deviation
+        c1_std, c2_std, c3_std = self.c1_feature[0], self.c2_feature[0], self.c3_feature[0]
+        c1q_std, c2q_std, c3q_std = query_image.c1_feature[0], query_image.c2_feature[0], query_image.c3_feature[0]
+        if ((c1q_std < (beta * c1_std) or c1q_std > (c1_std / beta)) and (
+                c2q_std < (beta * c2_std) or c2q_std > (c2_std / beta)) or c3q_std < (
+                beta * c3_std) or c3q_std > (c3_std / beta)):
+            # no longer consider image
+            self.distance = -1
+        else:
+            total_distance = 0
+            for i in range(4):
+                c1_diff = np.sum((np.subtract(query_image.c1_feature[i + 5], self.c1_feature[i + 5])) ** 2)
+                c2_diff = np.sum((np.subtract(query_image.c2_feature[i + 5], self.c2_feature[i + 5])) ** 2)
+                c3_diff = np.sum((np.subtract(query_image.c3_feature[i + 5], self.c3_feature[i + 5])) ** 2)
+                sub_total = (wc1 * c1_diff) + (wc2 * c2_diff) + (wc3 * c3_diff)
+                total_distance += (weights[i] * sub_total)
+            self.distance = total_distance
+
+    def image_distance(self, query_image, weights, wc1, wc2, wc3, percent=50):
+        beta = 1 - percent / 100
+        # filter based on standard deviation
+        c1_std, c2_std, c3_std = self.c1_feature[0], self.c2_feature[0], self.c3_feature[0]
+        c1q_std, c2q_std, c3q_std = query_image.c1_feature[0], query_image.c2_feature[0], query_image.c3_feature[0]
+        if ((c1q_std < (beta * c1_std) or c1q_std > (c1_std / beta)) and (
+                c2q_std < (beta * c2_std) or c2q_std > (c2_std / beta)) or c3q_std < (
+                beta * c3_std) or c3q_std > (c3_std / beta)):
+            # no longer consider image
+            self.distance = -1
+        else:
+            total_distance = 0
+            for i in range(4):
+                c1_diff = np.sum((np.subtract(query_image.c1_feature[i + 1], self.c1_feature[i + 1])) ** 2)
+                c2_diff = np.sum((np.subtract(query_image.c2_feature[i + 1], self.c2_feature[i + 1])) ** 2)
+                c3_diff = np.sum((np.subtract(query_image.c3_feature[i + 1], self.c3_feature[i + 1])) ** 2)
+                sub_total = (wc1 * c1_diff) + (wc2 * c2_diff) + (wc3 * c3_diff)
+                total_distance += (weights[i] * sub_total)
+            self.distance = total_distance
